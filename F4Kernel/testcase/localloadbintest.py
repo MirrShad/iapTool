ab074b618045e3cbaea1a45414feb442ca975dba
XiaoxingChen <cxx@seer-robotics.com>
2018-03-15 13:01:12 +0800

    Modify: add iaptooltest submodule

--- F4Kernel/testcase/localloadbintest.py
+++ F4Kernel/testcase/localloadbintest.py
@@ -9,31 +9,33 @@ from time import sleep
 from chardev.udpchardev import UdpCharDev
 from F4Kernel.f4kerneliapdev import CF4KernelIapDev
 
-try:
-    with open('..\\User\\ipconfig.json', 'r') as f:
-        data = json.load(f)
-        f.close()
-    F4K_ip = data['inet addr']
-
-except:
-    #F4K_ip = '192.168.192.4'
-    F4K_ip = '127.0.0.1'
+F4K_ip = '127.0.0.1'
+IAP_UDP_PORT = 5000
 
 if(2 == len(sys.argv)):
     bin_file = sys.argv[1]
-    #*.gyro.bin
-    tail2 = bin_file[-7:-4]
-    if tail2 != '.sc':
-        print('not the firmware for seer controller, press enter to continue...')
-        input()
-        sys.exit()
-else:
-    bin_file = '../Output/Project.bin'
+
+    (tail2, tail) = bin_file.split('.')[-2:]
+    if(tail != 'bin'):
+        print('firmware should be *.bin, but current: ' + tail)
+        sleep(2)
+        sys.exit(1)
+
+    if(tail2 == 'sc'):
+        IAP_UDP_PORT = 5000
+        print('firmware for SRC1100, IAP port: %d'%IAP_UDP_PORT)
+    elif (tail2 == 'sc2'):
+        IAP_UDP_PORT = 6000
+        print('firmware for SRC1100, IAP port: %d'%IAP_UDP_PORT)
+    else:
+        print('unknow firmware type, should be *.sc.bin or *.sc2.bin, current: ' + tail2)
+        sleep(2)
+        sys.exit(1)
 
 BOOTLOADER_START_ADDR = 0x08000000
 BOOTPARAM_ADDR = 0x0800C000
 APP_START_ADDR = 0x08010000
-chardev = UdpCharDev((F4K_ip, 15003), (F4K_ip, 5000))
+chardev = UdpCharDev((F4K_ip, 15003), (F4K_ip, IAP_UDP_PORT))
 udpIapDev = CF4KernelIapDev(chardev)
 udpIapDev.settargetboardbootloader()
 FWV = udpIapDev.getbootloaderversion()
